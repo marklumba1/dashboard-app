0262203c9a07c6e4b222dfde888290a8938cd0f8
Mark Lumba <Mark.Lumba.-ND@disney.com>
2026-03-08 09:45:49 -0400

    added hydration boundary

--- hooks/useFetchUser.ts
+++ hooks/useFetchUser.ts
@@ -5,8 +5,6 @@ export const useFetchUser = () => {
     return useQuery({
         queryKey: ["user"],
         queryFn: () => fetchUser(),
-        staleTime: Infinity,        // user never becomes stale
-    refetchOnMount: false,      // don't refetch when component mounts
-    refetchOnWindowFocus: false // don't refetch when switching tabs
+        staleTime: Infinity,       
     })
 }
